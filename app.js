cc0623ef3dda3ea96f028e2076fe435f512994b2
Shane EC Davis <secd0183@yahoo.com>
2018-04-28 03:51:39 -0500

    UI finished starting app.js file now for function

---
+++ app.js
@@ -0,0 +1,27 @@
+//Define UI Vars
+const form = document.querySelector('#task-form');
+const taskList = document.querySelector('.collection');
+const clearBtn = document.querySelector('.clear-tasks');
+const filter = document.querySelector('#filter');
+const taskInput = document.querySelector('#task');
+
+//Load all event listeners
+loadEventListeners(); 
+
+//Load all event listeners
+function loadEventListeners(){
+  // Add task event
+  form.addEventListener('submit',addTask); 
+}
+
+//Add task
+function addTask(e){
+  if(taskInput.value === ''){
+    alert('Add a task');
+  }
+    const li = document.createElement('li');
+    // add class
+    li.className = 'collection-item';
+
+  e.preventDefault(); 
+}
